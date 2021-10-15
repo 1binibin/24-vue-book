4e3d332062be3bf43dc7f722958dde4936366837
1binibin <namwonbin@gmail.com>
2021-10-15 11:08:04 +0900

    use dotenv

--- src/modules/common.js
+++ src/modules/common.js
@@ -1,5 +1,6 @@
 const API_KEY = "1f86a6e1-6392-4a3f-959c-cc55a881de17";
 const URL_BOOKS = "http://127.0.0.1:3100/book";
 const URL_VIEW = "http://127.0.0.1:3100/view";
+const EXPRESS = "http://127.0.0.1:3000";
 
-export { API_KEY, URL_BOOKS, URL_VIEW };
+export { API_KEY, URL_BOOKS, URL_VIEW, EXPRESS };
